5bdacc7d13de6a40374a25f45ee6527cd13070ae
unknown <anasouza.0309@gmail.com>
2023-09-11 13:40:11 -0300

    [000]- AP - Setting the initial project

---
+++ src/main/java/com/example/redis/demorediscache/DemoRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis.demorediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRedisCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redis/demorediscache/DemoRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis.demorediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
